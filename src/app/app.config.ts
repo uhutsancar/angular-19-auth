e7071f1f9d60b366892c928c44e88aac4eceec9e
Uhut Sancar <sancaruhut@gmail.com>
2025-05-05 22:24:02 +0300

    authorization process

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { pathInterceptor } from '../interceptors/path-interceptor';
+import { API_URL } from '../data-access/base-api';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideHttpClient(withInterceptors([pathInterceptor])),
+
+    {
+      provide: API_URL,
+      useValue: environment.apiUrl,
+    },
+
+
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../components/login/login.component';
+import { HomeComponent } from '../components/home/home.component';
+import { authGuard } from '../guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        canMatch: [authGuard]
+    },
+
+];
